c41f2388c7809715f5b67ee46732694ceecb2291
tasty-tea <mrarttermos@gmail.com>
2020-10-30 18:35:03 +0600

    Added polls hook with partial render

--- lib/polls_hook_listener.rb
+++ lib/polls_hook_listener.rb
@@ -1,9 +1,8 @@
 class PollsHookListener < Redmine::Hook::ViewListener
-  def view_projects_show_left(context = {})
-    return content_tag("p", "Custom content added to the left")
-  end
+  render_on :view_projects_show_left, partial: "polls/project_overview"
 
   def view_projects_show_right(context = {})
     return content_tag("p", "Custom content added to the right")
   end
+  
 end
